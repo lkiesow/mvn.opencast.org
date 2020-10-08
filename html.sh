aa004d6b4decaebd9c270e944ff7dd595d118622
Matthias Neugebauer <matthias.neugebauer@uni-muenster.de>
2020-10-08 16:56:30 +0200

    Add net.sourceforge.nekohtml:com.springsource.org.cyberneko.html:1.9.13

--- html.sh
+++ html.sh
@@ -3,7 +3,7 @@
 echo '<html>' > index.html
 echo '<head><title>mvn.opencast.org</title></head>' >> index.html
 echo '<body><h1>mvn.opencast.org</h1><ul>' >> index.html
-for f in $(git ls-files com org | sort); do
+for f in $(git ls-files com net org | sort); do
 	echo "<li><a href='${f}'>${f}</a></li>" >> index.html
 done
 echo '</ul></body></html>' >> index.html
